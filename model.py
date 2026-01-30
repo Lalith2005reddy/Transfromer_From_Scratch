a1aa4e8f4dfca3d5d91968eb129b58621db79c76
Lalith2005reddy <lalithreddyarani@gmail.com>
2026-01-30 23:04:03 +0530

    Add & Norm Layer

--- model.py
+++ model.py
@@ -24,3 +24,41 @@ class PositionalEncodings(nn.Module):
 
         pe = torch.zeros(seq_len,d_model)
 
+        #create a vector of size (seq_len,1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0,d_model,2).float() * math.log(10000.0) / d_model)
+
+        #Apply the sin to even pos
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0) # (1, Seq_len, d_model)
+
+        self.register_buffer('pe',pe)
+
+    def forward(self,x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
+    
+class LayerNormalization(nn.Module):
+
+    def __init__(self, eps : float = 10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) #Multiplied
+        self.bias = nn.Parameter(torch.zeros(1)) #Added
+
+    def forward(self,x):
+        mean = x.mean(dim = -1, keepdim=True)
+        std = x.std(dim= -1, keepdim = True)
+        return self.alpha * (x-mean) / (std + self.eps) + self.bias
+    
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_model : int, d_ff : int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff,d_model)
+    
+    def forward(self, x):
+        return self.linear_2(self.dropout(torch.relu(self.linear_1)))
