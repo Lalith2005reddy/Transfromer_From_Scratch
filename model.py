3b262a5d9638124e515c9e488add161e2a5830cf
Lalith2005reddy <lalithreddyarani@gmail.com>
2026-01-30 20:13:16 +0530

    first commit

---
+++ model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self,d_model : int,vocab_size : int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocal_size = vocab_size
+        self.embeddings = nn.Embedding(vocab_size,d_model)
+
+    def forward(self, x):
+        return self.embeddings(x) * math.sqrt(self.d_model)
+    
+class PositionalEncodings(nn.Module):
+
+    def __init__(self,d_model : int,seq_len : int,dropout : float):
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+
+        pe = torch.zeros(seq_len,d_model)
+
